f78fa615a612cd81f9770d3e27703cb3c83e4903
aidan-starke <starke.aidan@gmail.com>
2022-06-13 11:32:43 +1200

    prettier

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -2,12 +2,12 @@ const { join } = require("path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(__dirname, "pages/**/*.{js,ts,jsx,tsx}"),
-    join(__dirname, "components/**/*.{js,ts,jsx,tsx}"),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		join(__dirname, "pages/**/*.{js,ts,jsx,tsx}"),
+		join(__dirname, "components/**/*.{js,ts,jsx,tsx}"),
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 };
