c4799cbe4cb9b75c6b63f34b895eb6e3575467be
aidan-starke <starke.aidan@gmail.com>
2022-06-14 14:29:57 +1200

    Set up `proposal-relayer`

---
+++ packages/proposal-relayer/commands/proposal-relayer.ts
@@ -0,0 +1,17 @@
+import { CENNZ_NETWORK } from "@gov-libs/lib/constants";
+import { getLogger } from "@gov-libs/utils/getLogger";
+import { getCENNZnetApi } from "@gov-libs/utils/getCENNZnetApi";
+import * as chalk from "chalk";
+import startProposalRelayer from "@proposal-relayer/utils/startProposalRelayer";
+
+const logger = getLogger("ProposalRelayer");
+logger.info(
+	`Start ProposalRelayer for CENNZnet ${chalk.magenta(
+		"%s"
+	)}...`,
+	CENNZ_NETWORK,
+);
+
+getCENNZnetApi().then(startProposalRelayer).catch((error) => {
+		logger.error("%s", error);
+	});
---
+++ packages/proposal-relayer/utils/startProposalRelayer.ts
@@ -0,0 +1,5 @@
+import type { Api } from "@cennznet/api";
+
+export default async function startProposalRelayer(cennzApi: Api): Promise<void> {
+	console.log(cennzApi.isConnected);
+}
