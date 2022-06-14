3c23e48299444928825d135337051bf319786e57
aidan-starke <starke.aidan@gmail.com>
2022-06-15 10:53:04 +1200

    add `requeueMessage` util

--- packages/gov-libs/lib/constants.ts
+++ packages/gov-libs/lib/constants.ts
@@ -6,3 +6,4 @@ export const RABBBITMQ_SERVER: string =
 	process.env.RABBBITMQ_SERVER || "amqp://guest:guest@localhost:5672";
 
 export const MESSAGE_MAX_TIME = Number(process.env.MESSAGE_MAX_TIME || 30000);
+export const MESSAGE_MAX_RETRY = Number(process.env.MESSAGE_MAX_RETRY ?? 3);
