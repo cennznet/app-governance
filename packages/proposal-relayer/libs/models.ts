550155ddd8967aeae6cc0bb768c48b82172615f9
aidan-starke <starke.aidan@gmail.com>
2022-06-15 10:57:56 +1200

    Add `state`, `status` to `ProposalInterface`

--- packages/proposal-relayer/libs/models.ts
+++ packages/proposal-relayer/libs/models.ts
@@ -6,14 +6,17 @@ mongoose.connect(MONGODB_SERVER);
 export interface ProposalInterface {
 	proposalId: number;
 	proposalDescription?: string;
-	proposalJustificationUri;
-	string;
+	proposalJustificationUri: string;
+	state: "Created" | "InfoFetched" | "DiscordSent";
+	status: "Pending" | "Successful" | "Failed" | "Skipped" | "Aborted";
 }
 
 const ProposalSchema = new Schema<ProposalInterface>({
 	proposalId: { type: Schema.Types.Number, required: true, unique: true },
 	proposalDescription: { type: Schema.Types.String },
 	proposalJustificationUri: { type: Schema.Types.String },
+	state: { type: Schema.Types.String, required: true },
+	status: { type: Schema.Types.String, required: true },
 });
 
 export const Proposal: Model<ProposalInterface> = mongoose.model(
