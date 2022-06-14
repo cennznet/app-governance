5f2f655b2edfaed04294a129e6ea8716ff904fdd
aidan-starke <starke.aidan@gmail.com>
2022-06-15 10:04:21 +1200

    Add MONGODB & RABBITMQ server constants, Set default CENNZ_NETWORK to `rata`

--- packages/gov-libs/lib/constants.ts
+++ packages/gov-libs/lib/constants.ts
@@ -1,1 +1,6 @@
-export const CENNZ_NETWORK: string = process.env.CENNZ_NETWORK || "nikau";
+export const CENNZ_NETWORK: string = process.env.CENNZ_NETWORK || "rata";
+
+export const MONGODB_SERVER: string =
+	process.env.MONGODB_SERVER || "mongodb://root:root@localhost:27017/test";
+export const RABBBITMQ_SERVER: string =
+	process.env.RABBBITMQ_SERVER || "amqp://guest:guest@localhost:5672";
