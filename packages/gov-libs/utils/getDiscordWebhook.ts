a86112043f2c9ce179c3925da8b31d1f5bcf1553
aidan-starke <starke.aidan@gmail.com>
2022-06-15 13:40:49 +1200

    Get Discord Webhook

--- packages/gov-libs/utils/getDiscordWebhook.ts
+++ packages/gov-libs/utils/getDiscordWebhook.ts
@@ -1,7 +1,11 @@
 import type { InteractionWebhook } from "discord.js";
 
 import { Client } from "discord.js";
+import { getLogger } from "@gov-libs/utils/getLogger";
+import { waitFor } from "@gov-libs/utils/waitFor";
 import { DISCORD_BOT } from "@gov-libs/lib/constants";
+
+const logger = getLogger("DiscordBot");
 
 const bot: Client<true> = new Client({
 	partials: ["MESSAGE", "CHANNEL", "REACTION"],
@@ -14,16 +18,31 @@ const bot: Client<true> = new Client({
 	],
 });
 
+bot.login(DISCORD_BOT.Token);
+
 export async function getDiscordWebhook(): Promise<InteractionWebhook> {
-	const channel: any = bot.channels.cache.get(DISCORD_BOT.ChannelId);
+	let webhook: InteractionWebhook;
 
-	if (DISCORD_BOT.WebhookId) 
-		return channel
-			.fetchWebhooks()
-			.then((hooks) => hooks.find((hook) => hook.id === DISCORD_BOT.WebhookId));
-	
+	bot.on("ready", async () => {
+		const channel: any = bot.channels.cache.get(DISCORD_BOT.ChannelId);
 
-	return await channel.createWebhook(DISCORD_BOT.BotName, {
-		avatar: DISCORD_BOT.AvatarUrl,
+		if (DISCORD_BOT.WebhookId)
+			webhook = channel
+				.fetchWebhooks()
+				.then((hooks) =>
+					hooks.find((hook) => hook.id === DISCORD_BOT.WebhookId)
+				);
+		else
+			webhook = await channel.createWebhook(DISCORD_BOT.BotName, {
+				avatar: DISCORD_BOT.AvatarUrl,
+			});
 	});
+
+	while (!webhook) {
+		logger.info("DiscordBot initializing...");
+		await waitFor(5);
+	}
+
+	logger.info("DiscordBot ready!");
+	return webhook;
 }
