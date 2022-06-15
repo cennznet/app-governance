accfb90702b934c742237d4dbc2e43123847b859
aidan-starke <starke.aidan@gmail.com>
2022-06-16 07:23:00 +1200

    Set up `docker-compose` for MongoDB & RabbitMQ

--- packages/gov-libs/lib/constants.ts
+++ packages/gov-libs/lib/constants.ts
@@ -1,7 +1,7 @@
 export const CENNZ_NETWORK: string = process.env.CENNZ_NETWORK || "rata";
 
 export const MONGODB_SERVER: string =
-	process.env.MONGODB_SERVER || "mongodb://localhost:27017/test";
+	process.env.MONGODB_SERVER || "mongodb://root:root@localhost:27017/admin";
 export const RABBBITMQ_SERVER: string =
 	process.env.RABBBITMQ_SERVER || "amqp://guest:guest@localhost:5672";
 
