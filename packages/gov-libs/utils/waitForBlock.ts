be67e0191c3eaf96be96dede941cd104a62115d6
aidan-starke <starke.aidan@gmail.com>
2022-06-21 08:25:58 +1200

    Specify `eslint-disable` rules

--- packages/gov-libs/utils/waitForBlock.ts
+++ packages/gov-libs/utils/waitForBlock.ts
@@ -2,10 +2,12 @@ import { Api } from "@cennznet/api";
 
 export async function waitForBlock(
 	api: Api,
+	// eslint-disable-next-line @typescript-eslint/no-inferrable-types
 	numberOfBlocks: number = 1
 ): Promise<void> {
 	let firstBlock: number;
 
+	// eslint-disable-next-line no-async-promise-executor
 	return new Promise(async (resolve) => {
 		const unsubscribe = await api.derive.chain.subscribeNewHeads(
 			async (header) => {
