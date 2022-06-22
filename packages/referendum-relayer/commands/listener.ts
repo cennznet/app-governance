4aaaa32e61ef325eff57d8d0de0e1b3de9ed5916
aidan-starke <starke.aidan@gmail.com>
2022-06-22 12:24:57 +1200

    Refactor `monitorVetoSum`

--- packages/referendum-relayer/commands/listener.ts
+++ packages/referendum-relayer/commands/listener.ts
@@ -1,14 +1,12 @@
-import type { Permill, ReferendumVoteCount, StorageKey } from "@cennznet/types";
-
 import chalk from "chalk";
 import { AMQPError } from "@cloudamqp/amqp-client";
 import { CENNZ_NETWORK } from "@gov-libs/constants";
 import { getLogger } from "@gov-libs/utils/getLogger";
 import { waitForBlock } from "@gov-libs/utils/waitForBlock";
-import { Referendum } from "@referendum-relayer/libs/models";
 import { getRabbitMQSet } from "@gov-libs/utils/getRabbitMQSet";
 import { getCENNZnetApi } from "@gov-libs/utils/getCENNZnetApi";
 import { BLOCK_POLLING_INTERVAL } from "@referendum-relayer/libs/constants";
+import { monitorVetoSum } from "@referendum-relayer/libs/utils/monitorVetoSum";
 
 const logger = getLogger("ReferendumListener");
 logger.info(
@@ -25,30 +23,7 @@ Promise.all([getCENNZnetApi()]).then(async ([cennzApi]) => {
 		while (polling) {
 			await waitForBlock(cennzApi, BLOCK_POLLING_INTERVAL);
 
-			await cennzApi.query.governance.referendumVetoSum.entries(
-				(
-					entries: [
-						storageKey: StorageKey,
-						voteCount: ReferendumVoteCount["vote"]
-					][]
-				) => {
-					entries.forEach(async ([storageKey, voteCount]) => {
-						const proposalId = Number(storageKey.toHuman()[0]);
-						const vetoSum = voteCount.toNumber();
-
-						const referendum = await Referendum.findOne({ proposalId });
-						if (referendum?.vetoSum === vetoSum) return;
-
-						logger.info(
-							"Referendum #%d: Update found, sent to queue...",
-							proposalId
-						);
-						queue.publish(JSON.stringify({ proposalId, vetoSum }), {
-							type: !referendum?.vetoSum ? "new" : "update",
-						});
-					});
-				}
-			);
+			await monitorVetoSum(cennzApi, queue);
 		}
 	} catch (error) {
 		if (error instanceof AMQPError) error?.connection?.close();
---
+++ packages/referendum-relayer/libs/utils/monitorVetoSum.ts
@@ -0,0 +1,38 @@
+import type { Api } from "@cennznet/api";
+import type { AMQPQueue } from "@cloudamqp/amqp-client";
+import type { ReferendumVoteCount, StorageKey } from "@cennznet/types";
+
+import { getLogger } from "@gov-libs/utils/getLogger";
+import { Referendum } from "@referendum-relayer/libs/models";
+
+const logger = getLogger("ReferendumListener");
+
+export async function monitorVetoSum(
+	cennzApi: Api,
+	queue: AMQPQueue
+): Promise<void> {
+	await cennzApi.query.governance.referendumVetoSum.entries(
+		(
+			entries: [
+				storageKey: StorageKey,
+				voteCount: ReferendumVoteCount["vote"]
+			][]
+		) => {
+			entries.forEach(async ([storageKey, voteCount]) => {
+				const proposalId = Number(storageKey.toHuman()[0]);
+				const vetoSum = voteCount.toNumber();
+
+				const referendum = await Referendum.findOne({ proposalId });
+				if (referendum?.vetoSum === vetoSum) return;
+
+				logger.info(
+					"Referendum #%d: Update found, sent to queue...",
+					proposalId
+				);
+				queue.publish(JSON.stringify({ proposalId, vetoSum }), {
+					type: !referendum?.vetoSum ? "new" : "update",
+				});
+			});
+		}
+	);
+}
