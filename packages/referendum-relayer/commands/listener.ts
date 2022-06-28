ffe4f688b39610fef1d6c703873beb3638043510
aidan-starke <starke.aidan@gmail.com>
2022-06-29 10:28:04 +1200

    `ReferendumListener` - swap `while` loop for `subscribeFinalizedHeads`

--- packages/referendum-relayer/commands/listener.ts
+++ packages/referendum-relayer/commands/listener.ts
@@ -2,7 +2,6 @@ import chalk from "chalk";
 import { AMQPError } from "@cloudamqp/amqp-client";
 import { CENNZ_NETWORK } from "@gov-libs/constants";
 import { getLogger } from "@gov-libs/utils/getLogger";
-import { waitForBlock } from "@gov-libs/utils/waitForBlock";
 import { getRabbitMQSet } from "@gov-libs/utils/getRabbitMQSet";
 import { getCENNZnetApi } from "@gov-libs/utils/getCENNZnetApi";
 import { BLOCK_POLLING_INTERVAL } from "@referendum-relayer/libs/constants";
@@ -15,19 +14,28 @@ logger.info(
 );
 
 Promise.all([getCENNZnetApi()]).then(async ([cennzApi]) => {
-	const polling = true;
+	let lastBlockPolled: number;
 
 	const [, queue] = await getRabbitMQSet("ReferendumQueue");
 
-	try {
-		while (polling) {
-			await waitForBlock(cennzApi, BLOCK_POLLING_INTERVAL);
+	cennzApi.rpc.chain
+		.subscribeFinalizedHeads(async (head) => {
+			const blockNumber = head.number.toNumber();
+			if (
+				lastBlockPolled &&
+				blockNumber < lastBlockPolled + BLOCK_POLLING_INTERVAL
+			)
+				return;
+
+			lastBlockPolled = blockNumber;
+			logger.info(`HEALTH CHECK => ${chalk.italic.green("OK")}`);
+			logger.info(`At blocknumber: ${chalk.italic.gray("%s")}`, blockNumber);
 
 			await monitorVetoSum(cennzApi, queue);
-		}
-	} catch (error) {
-		if (error instanceof AMQPError) error?.connection?.close();
-		logger.error("%s", error);
-		process.exit(1);
-	}
+		})
+		.catch((error) => {
+			if (error instanceof AMQPError) error?.connection?.close();
+			logger.error("%s", error);
+			process.exit(1);
+		});
 });
